8017883fc87cfd3fcde0b6e2a84cc9b7d46e083d
Kevin Harrington <mad.hephaestus@gmail.com>
2023-05-18 21:07:13 -0400

    voices

--- TextToSpeech.groovy
+++ TextToSpeech.groovy
@@ -3,7 +3,10 @@ BowlerKernel.speak("This is the default voice");
 BowlerKernel.speak("Voice one", 100, 0, 300, 1.0, 1.0);
 BowlerKernel.speak("Voice two", 100, 0, 200, 1.0, 1.0);
 BowlerKernel.speak("Voice three", 100, 0, 100, 1.0, 1.0);
-BowlerKernel.speak("Voice four", 100, 0, 50, 1.0, 1.0);
+BowlerKernel.speak("Voice four", 100, 0, 400, 1.0, 1.0);
+BowlerKernel.speak("Voice five", 100, 0, 500, 1.0, 1.0);
+BowlerKernel.speak("Voice six", 100, 0, 600, 1.0, 1.0);
+BowlerKernel.speak("Voice seven", 100, 0, 700, 1.0, 1.0);
 BowlerKernel.speak(
 	"we are the borg; rsistnce is feutle",
 	100, //rate 
